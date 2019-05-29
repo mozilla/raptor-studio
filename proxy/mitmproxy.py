c53bcfcdd89312a3e247842f8470bf157a1f762a
Florin Strugariu <bebe@mozilla.ro>
2019-05-29 11:27:52 +0300

    Add mitmproxy 4.0.4 and wpr (#14)

---
+++ proxy/mitmproxy.py
@@ -0,0 +1,125 @@
+import os
+import signal
+import subprocess
+import sys
+
+dirname = os.path.dirname(__file__)
+
+
+class MITMProxyBase(object):
+    def __init__(self, path, version, mode):
+        self.path = path
+        self.mode = mode
+        self.version = version
+        self.mitm_home = os.path.join(os.path.expanduser("~"), ".mitmproxy")
+        self.cert = os.path.join(self.mitm_home, "mitmproxy-ca-cert.cer")
+        self.scripts = os.path.join(dirname, "scripts")
+        self.binary_path = os.path.join(dirname, "utils", "mitm%s" % self.version)
+
+    @property
+    def binary(self):
+        if "linux" in sys.platform:
+            name = "mitmdump-linux"
+        elif "darwin" in sys.platform:
+            name = "mitmdump-osx"
+        elif "win" in sys.platform:
+            name = "mitmdump-win.exe"
+        return os.path.join(self.binary_path, name)
+
+    def start(self):
+        print("Starting MitmProxy")
+        print(" ".join(self.command()))
+
+        logfile = open(os.path.join(dirname, "mitmproxy.log"), "w")
+
+        self.process = subprocess.Popen(self.command(), stdout=logfile, stderr=logfile)
+
+        return self.process
+
+    def stop(self):
+        self.process.send_signal(signal.SIGINT)
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, *args):
+        try:
+            self.process.wait()
+        finally:
+            self.stop()
+
+
+class MITMProxy202(MITMProxyBase):
+    def __init__(self, path, mode="record"):
+        MITMProxyBase.__init__(self, path=path, version="2.0.2", mode=mode)
+
+    def command(self):
+        if self.mode is "record":
+            command = [
+                self.binary,
+                "--wfile",
+                self.path,
+                "--script",
+                " ".join([os.path.join(self.scripts, "inject-deterministic.py")]),
+            ]
+        elif self.mode is "replay":
+            command = [
+                self.binary,
+                "--replay-kill-extra",
+                "--script",
+                " ".join(
+                    [
+                        os.path.join(self.scripts, "alternate-server-replay-2.0.2.py"),
+                        self.path,
+                    ]
+                ),
+            ]
+        else:
+            raise Exception("Unknown proxy mode! Proxy mode: %s" %self.mode)
+        return command
+
+
+class MITMProxy404(MITMProxyBase):
+    def __init__(self, path, mode="record"):
+        MITMProxyBase.__init__(self, path=path, version="4.0.4", mode=mode)
+
+    def command(self):
+        if self.mode is "record":
+            command = [
+                self.binary,
+                "--save-stream-file",
+                self.path,
+                "--scripts",
+                os.path.join(self.scripts, "inject-deterministic.py"),
+            ]
+        elif self.mode is "replay":
+            command = [
+                self.binary,
+                "--scripts",
+                os.path.join(self.scripts, "alternate-server-replay-4.0.4.py"),
+                "--set",
+                "server_replay={}".format(self.path),
+            ]
+        elif self.mode is "forward":
+            command = [
+                self.binary,
+                "--listen-host",
+                "127.0.0.1",
+                "--listen-port",
+                "8080",
+                "--scripts",
+                os.path.join(self.scripts, "mitm_port_fw.py"),
+                "--set",
+                "portmap=80:8081,443:8082",
+                # For more information see ssl_insecure and ssl_verify_upstream_trusted_ca on
+                # https://docs.mitmproxy.org/stable/concepts-options/
+                # '--ssl-insecure',
+                "--set",
+                "ssl_verify_upstream_trusted_ca=%s"
+                % os.path.join(self.mitm_home, "mitmproxy-ca.pem"),
+            ]
+        else:
+            raise Exception("Unknown proxy mode! Proxy mode: %s" %self.mode)
+
+        return command
--- studio.py
+++ studio.py
@@ -2,22 +2,30 @@ import click
 import click_config_file
 
 from apps import Firefox, GeckoViewExample, Chrome
-from mitmproxy import MITMProxy
+from proxy.mitmproxy import MITMProxy202, MITMProxy404
+from proxy.webpagereplay import WebPageReplay
 
 APPS = {"Firefox": Firefox, "GeckoViewExample": GeckoViewExample, "Chrome": Chrome}
+PROXYS = {"mitm2": MITMProxy202, "mitm": MITMProxy404, "wpr": WebPageReplay}
 
 
 @click.command()
 @click.option(
     "--app", required=True, type=click.Choice(APPS.keys()), help="App to launch."
 )
+@click.option(
+    "--proxy",
+    required=True,
+    type=click.Choice(PROXYS.keys()),
+    help="Proxy Service to use.",
+)
 @click.option("--record/--replay", default=False)
 @click.option("--certutil", help="Path to certutil.")
 @click.option("--url", default="about:blank", help="Site to load.")
 @click.argument("path")
 @click_config_file.configuration_option()
-def cli(app, record, certutil, url, path):
-    with MITMProxy(path, record) as proxy:
+def cli(app, proxy, record, certutil, url, path):
+    with PROXYS[proxy](path=path, mode="record" if record else "replay") as proxy:
         app = APPS[app](proxy, certutil)
         app.start(url)
 
